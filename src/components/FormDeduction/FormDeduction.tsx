30182faedb69bffd4726d8488274d5398d7067b3
mikhailkaryamin <karyamin.mihail@yandex.ru>
2020-10-31 17:13:29 +0500

    fix(FormDeduction): add div container on form-deduction__title form-deduction__group-buttons

--- src/components/FormDeduction/FormDeduction.tsx
+++ src/components/FormDeduction/FormDeduction.tsx
@@ -23,23 +23,25 @@ const FormDeduction: React.FC = () => {
         sum={78000}
         year={1}
       />
-      <strong className="form-deduction__title">
-        Что уменьшаем?
-      </strong>
-      <div className="form-deduction__group-buttons">
-        <Button
-          className={`form-deduction__button-pay`}
-          isVisibleTitle={true}
-          onClick={emptyFunc}
-          prefix={`decrease button--decrease-active`}
-          title={`Платёж`}
-        />
-        <Button
-          isVisibleTitle={true}
-          onClick={emptyFunc}
-          prefix={`decrease`}
-          title={`Срок`}
-        />
+      <div className="form-deduction__container-decrease">
+        <strong className="form-deduction__title">
+          Что уменьшаем?
+        </strong>
+        <div className="form-deduction__group-buttons">
+          <Button
+            className={`form-deduction__button-pay`}
+            isVisibleTitle={true}
+            onClick={emptyFunc}
+            prefix={`decrease button--decrease-active`}
+            title={`Платёж`}
+          />
+          <Button
+            isVisibleTitle={true}
+            onClick={emptyFunc}
+            prefix={`decrease`}
+            title={`Срок`}
+          />
+        </div>
       </div>
       <Button
         className={`form-deduction__button-submit`}
