13da0f2b639f1da877c3199c5aaf9b3971b88a2d
mikhailkaryamin <karyamin.mihail@yandex.ru>
2020-10-31 15:57:54 +0500

    feat(Input): add attribute disabled, required, add props className, isDisabled, isRequired

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,20 +1,27 @@
 import React from 'react';
 
 type Props = {
+  className?: string;
+  isDisabled?: boolean;
+  isRequired?: boolean;
   name: string;
   prefix?: string;
   placeHolder?: string;
   type: string;
 }
 
-const Input: React.FC<Props> = ({ placeHolder, prefix, type }: Props) => {
+const Input: React.FC<Props> = ({
+  className, isDisabled = false, name, placeHolder, prefix, isRequired = false, type
+}: Props) => {
   return (
     <input
-      className={`input ${prefix ? `${prefix}__input` : ``}`}
-      type={type}
+      className={`input ${prefix ? `input--${prefix}` : ``} ${className ? className : ``}`}
+      disabled={isDisabled}
       id={name}
       name={name}
       placeholder={`${placeHolder ? `${placeHolder}` : ``}`}
+      required={isRequired}
+      type={type}
     />
   );
 };
